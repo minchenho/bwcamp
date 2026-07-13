760f0e627b670b0e8731ced42bf0bc7444771372
Minuet <menuett@gmail.com>
2026-07-13 09:39:15 +0800

    trafficList

--- resources/views/backend/in_camp/trafficList.blade.php
+++ resources/views/backend/in_camp/trafficList.blade.php
@@ -8,55 +8,76 @@
             color: #33B2FF!important;
         }
     </style>
-    <h2>{{ $campFullData->abbreviation }} 交通名單</h2>
+    <h2>{{ $camp_info->abbreviation }} 交通名單</h2> 
+    
     @foreach ($batches as $batch)
-        <h4>梯次：{{ $batch->name }}<a href="{{ route("showTrafficList", $campFullData->id) }}?batch_id={{ $batch->id }}&download=1" align="left" class="btn btn-warning float-right">下載車資繳納明細</a></h4>
+        <hr>
+        <h4>
+            梯次：{{ $batch->name }}
+            <a href="{{ route('showTrafficList', $camp_info->id) }}?batch_id={{ $batch->id }}&download=1" class="btn btn-warning float-right">下載車資繳納明細</a>
+        </h4>
+
+        @php
+            // 從所有參加者中，過濾出當前特定梯次的人
+            $batchApplicants = $attendApplicants->where('batch_id', $batch->id);
+            
+            // 利用 Controller 傳來的閉包，即時計算該梯次的去回程統計
+            $current_traffic_depart = $getTrafficStats($batchApplicants, 'depart');
+            $current_traffic_return = $getTrafficStats($batchApplicants, 'return');
+            
+            $count_depart = 0;    
+            $count_return = 0;    
+        @endphp
+
         <h5>去程</h5>
         <table class="table table-bordered">
-            <tr>
-                <th>地點</th>
-                <th>人數</th>
-                <th>動作</th>
-            </tr>
-            @php
-                $count_depart = 0;    
-            @endphp
-            @foreach ($traffic_depart as $t)
+            <thead>
                 <tr>
-                    <td>{{ $t->traffic_depart }}</td>
-                    <td>{{ $t->count }}</td>
-                    <td><a href="{{ route("showTrafficListLoc", $campFullData->id) }}?batch_id={{ $batch->id }}&depart_from={{ $t->traffic_depart }}&download=0" class="btn btn-info">看名單</a>
-                    <a href="{{ route("showTrafficListLoc", $campFullData->id) }}?batch_id={{ $batch->id }}&depart_from={{ $t->traffic_depart }}&download=1" class="btn btn-warning">下載名單</a></td>
+                    <th>地點</th>
+                    <th>人數</th>
+                    <th>動作</th>
                 </tr>
-                @php
-                    $count_depart += $t->count;    
-                @endphp
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($current_traffic_depart as $t)
+                    <tr>
+                        <td>{{ $t['traffic_depart'] }}</td>
+                        <td>{{ $t['count'] }}</td>
+                        <td>
+                            <a href="{{ route('showTrafficListLoc', $camp_info->id) }}?batch_id={{ $batch->id }}&depart_from={{ urlencode($t['traffic_depart']) }}&download=0" class="btn btn-info">看名單</a>
+                            <a href="{{ route('showTrafficListLoc', $camp_info->id) }}?batch_id={{ $batch->id }}&depart_from={{ urlencode($t['traffic_depart']) }}&download=1" class="btn btn-warning">下載名單</a>
+                        </td>
+                    </tr>
+                    @php $count_depart += $t['count']; @endphp
+                @endforeach
+            </tbody>
         </table>
-        共 {{ $count_depart }} 位
+        <p>共 {{ $count_depart }} 位</p>
+
         <h5>回程</h5>
         <table class="table table-bordered">
-            <tr>
-                <th>地點</th>
-                <th>人數</th>
-                <th>動作</th>
-            </tr>
-            @php
-                $count_return = 0;    
-            @endphp
-            @foreach ($traffic_return as $t)
+            <thead>
                 <tr>
-                    <td>{{ $t->traffic_return }}</td>
-                    <td>{{ $t->count }}</td>
-                    <td><a href="{{ route("showTrafficListLoc", $campFullData->id) }}?batch_id={{ $batch->id }}&back_to={{ $t->traffic_return }}&download=0" class="btn btn-info">看名單</a>
-                    <a href="{{ route("showTrafficListLoc", $campFullData->id) }}?batch_id={{ $batch->id }}&back_to={{ $t->traffic_return }}&download=1" class="btn btn-warning">下載名單</a></td>
+                    <th>地點</th>
+                    <th>人數</th>
+                    <th>動作</th>
                 </tr>
-                @php
-                    $count_return += $t->count;    
-                @endphp
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($current_traffic_return as $t)
+                    <tr>
+                        <td>{{ $t['traffic_return'] }}</td>
+                        <td>{{ $t['count'] }}</td>
+                        <td>
+                            <a href="{{ route('showTrafficListLoc', $camp_info->id) }}?batch_id={{ $batch->id }}&back_to={{ urlencode($t['traffic_return']) }}&download=0" class="btn btn-info">看名單</a>
+                            <a href="{{ route('showTrafficListLoc', $camp_info->id) }}?batch_id={{ $batch->id }}&back_to={{ urlencode($t['traffic_return']) }}&download=1" class="btn btn-warning">下載名單</a>
+                        </td>
+                    </tr>
+                    @php $count_return += $t['count']; @endphp
+                @endforeach
+            </tbody>
         </table>
-        共 {{ $count_return }} 位
-        <br><br>
+        <p>共 {{ $count_return }} 位</p>
+        <br>
     @endforeach
 @endsection
